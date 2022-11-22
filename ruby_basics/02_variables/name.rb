d8629f02f55638c46fc9d56ffa7c41976d252591
Kelsi Olstad <kelsi@popcodes.com>
2022-11-22 16:13:19 -0700

    Complete variable exercises

---
+++ ruby_basics/02_variables/name.rb
@@ -0,0 +1,15 @@
+puts "What is your first name?"
+
+first_name = gets.chomp
+
+puts "What is your last name?"
+
+last_name = gets.chomp
+
+name = "#{first_name} #{last_name}"
+
+puts "Hello #{name}!"
+
+10.times do |n|
+    puts name
+end
